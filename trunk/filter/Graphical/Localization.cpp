42033782adc53b81100cd68323b9f5d0edbe5d0b
plumdeq <plumdeq@f775f7fb-c228-641e-0da4-618e932950d0>
2011-04-26 13:10:34 +0000

    It was so simple, 1D

--- trunk/filter/Graphical/Localization.cpp
+++ trunk/filter/Graphical/Localization.cpp
@@ -6,33 +6,34 @@
 
 int main(int argc, char* argv[]){
  
-    std::string setName(argv[1]);
-    DataReader dataObj1(setName);
-    int key;
+	std::string setName(argv[1]);
+	DataReader dataObj1(setName);
+	int key;
 
-	 float a = 16;
+	float a = 16;
 
-    /*Window initialization*/
-    dataObj1.initWindow();
-    dataObj1.clearImage();
-    
-	 dataObj1.readData();//read the next laser data
-    dataObj1.displayLaserData();//display the current laser data on the graphical window
-  	 dataObj1.printLaserData();//display the current laser data on the graphical window
+	/*Window initialization*/
+	dataObj1.initWindow();
+	dataObj1.clearImage();
 
-	 // Initialization of Kalman Filter
-	 float init_x=0;
-	 float init_std=0;
-	 float init_q=1;
-	 float init_r=5;
-	 Kalman x(init_x, init_std, init_q, init_r);
-    std::cout << "mean(I) = " << x.mean << " std(I) = " << x.std << std::endl << std::endl;
+	dataObj1.readData();//read the next laser data
+	dataObj1.displayLaserData();//display the current laser data on the graphical window
+	dataObj1.printLaserData();//display the current laser data on the graphical window
 
-    /*Displaying the dataset (each sliding window; each
-     frame)*/
-    while(dataObj1.readData() == NOERROR) {
-    	dataObj1.displayLaserData();//display the current laser data on the graphical window
-    	dataObj1.printLaserData();//print the current laser data
+	// Initialization of Kalman Filter
+	//float init_x=0;
+	float init_x = dataObj1.dataLaserR[BN / 2];
+	float init_std=0;
+	float init_q=1;
+	float init_r=5;
+	Kalman x(init_x, init_std, init_q, init_r);
+	std::cout << "mean(I) = " << x.mean << " std(I) = " << x.std << std::endl << std::endl;
+
+	/*Displaying the dataset (each sliding window; each
+	frame)*/
+	while(dataObj1.readData() == NOERROR) {
+		dataObj1.displayLaserData();//display the current laser data on the graphical window
+		dataObj1.printLaserData();//print the current laser data
 
 		// prediction phase
 		float a = 16;
@@ -41,14 +42,15 @@ int main(int argc, char* argv[]){
 		std::cout << "mean(P) = " << x.mean << " std(P) = " << x.std << std::endl;
 
 		// estimation phase
-		float o;
+		float o = dataObj1.dataLaserR[BN / 2];
+		//float o;
 		std::cout << "observation = " << o << std::endl;
 		x.estimation(o);
 		std::cout << "mean(E) = " << x.mean << " std(E) = " << x.std << std::endl << std::endl;
 
 		/*delay*/
 		key=cvWaitKey(0);
-    }
-    
-    return 0;
+	}
+
+	return 0;
 }
